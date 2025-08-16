b97b62964b62cab205281d6fac1b397cb0253bda
HuzaifaMehboob <huzaifameh1925@gmail.com>
2025-08-16 13:04:52 +0500

    folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "medical_chatbot",
+    version= "0.0.0",
+    author = "Huzaifa Mehboob",
+    author_email= "huzaifameh1925@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
